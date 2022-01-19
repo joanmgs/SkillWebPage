440b5738c4cdcd39544497fd06d906c41018d63b
joanmgs <joan.gs@outlook.com>
2022-01-19 16:49:23 -0500

    added key to all li tags

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,23 +13,23 @@ export default function Skills({ strengths }) {
 
       {/* Master */}
       { skills.master.length > 0 && <h4>Master</h4> }
-      { skills.master.length > 0 && <ul>{ skills.master.map( ({ name }) => <li>{ name }</li>) }</ul> }
+      { skills.master.length > 0 && <ul>{ skills.master.map( ({ name }, index) => <li key={index}>{ name }</li>) }</ul> }
 
       {/* Expert */}
       { skills.expert.length > 0 && <h4>Expert</h4> }
-      { skills.expert.length > 0 && <ul>{ skills.expert.map( ({ name }) => <li>{ name }</li>) }</ul> }
+      { skills.expert.length > 0 && <ul>{ skills.expert.map( ({ name }, index) => <li key={index}>{ name }</li>) }</ul> }
 
       {/* Proficient */}
       { skills.proficient.length > 0 && <h4>Proficient</h4> }
-      { skills.proficient.length > 0 && <ul>{ skills.proficient.map( ({ name }) => <li>{ name }</li>) }</ul> }
+      { skills.proficient.length > 0 && <ul>{ skills.proficient.map( ({ name }, index) => <li key={index}>{ name }</li>) }</ul> }
 
       {/* Novice */}
       { skills.novice.length > 0 && <h4>Novice</h4> }
-      { skills.novice.length > 0 && <ul>{ skills.novice.map( ({ name }) => <li>{ name }</li>) }</ul> }
+      { skills.novice.length > 0 && <ul>{ skills.novice.map( ({ name }, index) => <li key={index}>{ name }</li>) }</ul> }
 
       {/* No Experience, but interested */}
       { skills.noExperience.length > 0 && <h4>No experience, but interested</h4> }
-      { skills.noExperience.length > 0 && <ul>{ skills.noExperience.map( ({ name }) => <li>{ name }</li>) }</ul> }
+      { skills.noExperience.length > 0 && <ul>{ skills.noExperience.map( ({ name }, index) => <li key={index}>{ name }</li>) }</ul> }
     </>
   );
 }
